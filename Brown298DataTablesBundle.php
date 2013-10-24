2e59836bce5dad148c20e0fa168c4e265dacda6f
shaggy <shaggy@dl.(none)>
2013-10-23 18:38:52 -0700

    adding bundle definition

---
+++ Brown298DataTablesBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace Brown298\DataTablesBundle;
+
+use \Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DataTablesBundle
+ *
+ * @package Brown298\DataTablesBundle
+ * @author  John Brown <brown.john@gmail.com>
+ */
+class Brown298DataTablesBundle extends Bundle
+{
+
+}
