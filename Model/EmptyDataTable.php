fb108ed7ff6b6cbe9a5c929be7d4f415a58c1f64
John Brown <john.brown@partnerweekly.com>
2013-11-11 11:58:49 -0800

    fixing unit tests

--- Model/EmptyDataTable.php
+++ Model/EmptyDataTable.php
@@ -1,6 +1,9 @@
 <?php
 namespace Brown298\DataTablesBundle\Model;
 
+use Brown298\DataTablesBundle\Model\DataTable\AbstractDataTable as AbstractBaseDataTable;
+use Brown298\DataTablesBundle\Model\DataTable\DataTableInterface as BaseDataTableInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -9,7 +12,17 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @package Brown298\DataTablesBundle\Model
  * @authro  John Brown <brown.john@gmail.com>
  */
-class EmptyDataTable extends AbstractDataTable implements DataTableInterface
+class EmptyDataTable extends AbstractBaseDataTable implements BaseDataTableInterface
 {
-    
+    /**
+     * getData
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array|mixed
+     */
+    public function getData(Request $request)
+    {
+        return array();
+    }
 }
