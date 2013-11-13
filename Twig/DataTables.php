187a171b736077d4dfb68e49d9283f4bd0e7494d
John Brown <john.brown@partnerweekly.com>
2013-11-13 10:05:33 -0800

    adding custom params option to twig

--- Twig/DataTables.php
+++ Twig/DataTables.php
@@ -45,10 +45,49 @@ class DataTables extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'addDataTable' => new \Twig_Function_Method($this, 'addDataTable', array(
+            'addDataTable'     => new \Twig_SimpleFunction('addDataTable', array($this, 'addDataTable'), array(
                     'is_safe' => array('html')
                 )),
         );
+    }
+
+    /**
+     * getFilters
+     *
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            'convertFunctions' => new \Twig_SimpleFilter('convertFunctions', array($this, 'convertFunctions')),
+        );
+    }
+
+    /**
+     * convertFunctions
+     *
+     * @param $rawJson
+     *
+     * @return string
+     */
+    public function convertFunctions($rawJson)
+    {
+        $data        = json_decode($rawJson);
+        $valueArray  = array();
+        $replaceKeys = array();
+
+        foreach($data as $key => &$value) {
+            if (preg_match('/^function.*/', $value)) {
+                $valueArray[]  = $value;
+                $value         = "%{$key}%";
+                $replaceKeys[] = "\"{$value}\"";
+            }
+        }
+
+        $resultJson = json_encode($data);
+        $resultJson = str_replace($replaceKeys, $valueArray, $resultJson);
+
+        return $resultJson;
     }
 
     /**
@@ -84,6 +123,7 @@ class DataTables extends \Twig_Extension
                 'table_class'     => 'display dataTable table table-striped',
                 'aaData'          => null,
                 'twigVars'        => array(),
+                'customParams'    => array(),
             ),
             $params
         );
@@ -99,10 +139,67 @@ class DataTables extends \Twig_Extension
     public function renderJs()
     {
         $args = array_merge($this->params['twigVars'], array(
-                'columns' => $this->columns,
-                'params'  => $this->params,
+                'columns'   => $this->columns,
+                'rawParams' => $this->params,
+                'params'    => $this->buildJsParams(),
             ));
+
         return $this->environment->render($this->params['script_template'], $args);
+    }
+
+    /**
+     * buildJsParams
+     *
+     */
+    protected function buildJsParams()
+    {
+        $keys    = array_merge(array(
+            'aaData',
+            'aaSorting',
+            'aaSortingFixed',
+            'aoColumnDefs',
+            'bLengthChange',
+            'bFilter',
+            'bPaginate',
+            'bProcessing',
+            'bServerSide',
+            'bSort',
+            'bInfo',
+            'fnDrawCallback',
+            'fnServerData',
+            'iDisplayLength',
+            'sPaginationType',
+        ), array_keys($this->params['customParams']));
+        $results = array();
+
+        // custom conversions
+        if (isset($this->params['path'])) {
+            $results['sAjaxSource'] = $this->params['path'];
+        }
+        if (isset($this->params['customSearchForm'])) {
+            $results['fnServerData'] = 'function ( sSource, aoData, fnCallback ) { var searchValues = jQuery("'
+                . $this->params['customSearchForm'] . '").serializeArray(); for ( var i=0; i < searchValues.length; i++)'
+                . ' { aoData.push(searchValues[i]); } jQuery.getJSON( sSource, aoData, function (json) {fnCallback(json)} ); }';
+        }
+        if (isset($this->params['aaData']) && isset($this->params['aoColumnDefs'])) {
+            $this->params['aoColumns'] = $this->params['aoColumnDefs'];
+            unset($this->params['aoColumnDefs']);
+        }
+
+        // build the final params
+        foreach ($keys as $key) {
+            if (isset($this->params[$key])) {
+                if (preg_match('/^aa.+', $key) || preg_match('/^ao.+', $key)) {
+                    $results[$key] = json_encode($this->params[$key]);
+                } elseif(isset($this->params['customParams'][$key])) {
+                    $results[$key] = $this->params['customParams'][$key];
+                } else {
+                    $results[$key] = $this->params[$key];
+                }
+            }
+        }
+
+        return $results;
     }
 
     /**
