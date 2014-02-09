4a109ffbd1a34347a3d99f40fa83bc7a909ad2de
John Brown <brown.john@gmail.com>
2014-02-09 10:36:29 +0000

    adding yml data table and beginning of xml

---
+++ Service/XmlTableBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: shaggy
+ * Date: 2/9/14
+ * Time: 1:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Brown298\DataTablesBundle\Service;
+
+
+class XmlTableBuilder {
+
+}
---
+++ Service/YmlTableBuilder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: shaggy
+ * Date: 2/9/14
+ * Time: 1:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Brown298\DataTablesBundle\Service;
+
+
+class YmlTableBuilder {
+
+}
